11fca20236cc5832a38a351c3dfd2d9f5c030424
021.898687 <021.898687@C305-SFT-038>
2024-08-07 14:21:25 -0300

    Adicionar arquivos de projeto.

---
+++ Atividades/Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double peso;
+        public double altura;
+
+        public Pessoa(string nome, int idade, double peso, double altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public void envelhecer()
+        {
+            while (idade < 120)
+            {
+                idade++;
+                if (idade < 21)
+                {
+                    altura += 0.5;
+                }
+            }
+
+            if (idade == 120)
+            {
+                Console.WriteLine("Essa pessoa aí morreu");
+            }
+            if (idade > 120)
+            {
+                Console.WriteLine("Esse caba aí é um vampiro");
+            }
+        }
+        public void engordar()
+        {
+        }
+        public void emagrecer()
+        {
+        }
+        public void crescer()
+        {
+        }
+    }
+}
---
+++ Atividades/Program.cs
@@ -0,0 +1,51 @@
+﻿using Atividades;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Qual o nome desse cabeça aí? ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Quantos anos você tem? ");
+        int idade;
+        while (!int.TryParse(Console.ReadLine(), out idade))
+        {
+            Console.Write("Por favor, insira uma idade válida: ");
+        }
+
+        Console.Write("Qual é o seu peso (em kg)? ");
+        double peso;
+        while (!double.TryParse(Console.ReadLine(), out peso))
+        {
+            Console.Write("Por favor, insira um peso válido: ");
+        }
+
+        Console.Write("Qual é a sua altura (em metros)? ");
+        double altura;
+        while (!double.TryParse(Console.ReadLine(), out altura))
+        {
+            Console.Write("Por favor, insira uma altura válida: ");
+        }
+
+        Pessoa pessoa = new Pessoa(nome, idade, peso, altura);
+
+        Console.WriteLine($"\nEntão as informações são:");
+        Console.WriteLine($"Nome: {pessoa.nome}");
+        Console.WriteLine($"Idade: {pessoa.idade}");
+        Console.WriteLine($"Peso: {pessoa.peso} kg");
+        Console.WriteLine($"Altura: {pessoa.altura} m");
+
+        Console.WriteLine("\nEntão, com o passar dos anos...");
+
+        pessoa.envelhecer();
+
+        Console.WriteLine($"\nDepois de envelhecer, as informações são:");
+        Console.WriteLine($"Nome: {pessoa.nome}");
+        Console.WriteLine($"Idade: {pessoa.idade}");
+        Console.WriteLine($"Peso: {pessoa.peso} kg");
+        Console.WriteLine($"Altura: {pessoa.altura} m");
+    }
+}
